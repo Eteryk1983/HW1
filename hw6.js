c89afe8fb907f64c03784f1c4b06a73c5dd828b8
terry <eht2009@yandex.ru>
2023-03-09 17:43:54 +0300

    change hw6

---
+++ hw6.js
@@ -0,0 +1,59 @@
+
+const nums = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 10) {
+    console.log(nums[i])
+    break;
+  }
+}
+
+let products = [1, 5, 4, 10, 0, 3];
+
+console.log(products [2]);
+
+let item = [1,3,5,10,20];
+item = item.join(' ');
+console.log(item);
+let arr = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1],
+];
+console.log(arr)
+
+let item5 = [1, 1, 1];
+item5.push('2,2,2');
+console.log(item5);
+
+let number = [9, 8, 7, 'a', 6, 5];
+number = number.sort();
+
+console.log(number);
+console.log(number.pop());
+
+
+
+
+let split = ['a', 'b', 'c', 'd', 'e', 'f'];
+split = split.reverse();
+console.log(split);
+
+let arr9 = [
+    [1, 2, 3],
+    [4, 5, 6],
+];
+for (let arr9In of arr9) {
+    for (let element of arr9In) {
+        
+    }
+};
+console.log(arr9);
+
+let products10 = [1, 2, 5, 3];
+for(let item of products10) {
+  
+
+};
+console.log (item);
+
